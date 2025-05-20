e22ae3b32afebde29ce7fba92b21f9d0a1d4bc7c
ASHISH KUMAR <152021705+LEYTHAN-X@users.noreply.github.com>
2025-05-20 13:12:29 +0530

    Initial commit before deployment

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import React, { useState } from 'react';
 import NavBar from './components/NavBar';
 import News from './components/News';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import LoadingBar from "react-top-loading-bar";
 
 const App = () => {
